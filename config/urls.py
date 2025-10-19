1f8c98354c5c7f9783b4fced6f732e1dd2db7709
Wale Johnson <Walejohnson1806@gmail.com>
2025-10-19 21:50:18 +0100

    fix- return proper 404 when film not found

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('films/', include('films.urls')), 
-
-    path('', RedirectView.as_view(url='/films/', permanent=False)),
+    path('api/', include('films.urls')),  # This adds /api/ prefix
+    
+    # API Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
