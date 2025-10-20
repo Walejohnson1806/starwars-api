ec3dcd7cec492e255fc69606d1b8bcab723d96ae
Wale Johnson <Walejohnson1806@gmail.com>
2025-10-20 10:33:28 +0100

    added Render domain to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security Settings
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-key-in-production-12345')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
